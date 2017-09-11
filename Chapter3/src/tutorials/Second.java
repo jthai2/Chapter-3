5bb486bbe45bfee5cc046181d6e3feac62ed294e
jthai2 <jimmythai35@gmail.com>
2017-09-11 09:47:04 -0500

    update tutorials

---
+++ Chapter3/src/tutorials/Second.java
@@ -0,0 +1,15 @@
+package tutorials;
+
+public class Second {
+
+	public static void displayAddress()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S. Hwy 14");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java application");
+
+	}
+
+}
---
+++ Chapter3/src/youDoIt/ParadiseInfo.java
@@ -0,0 +1,10 @@
+package youDoIt;
+
+public class ParadiseInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
