5624a324c9217361613ec7540c20aa75da601031
jthai2 <jimmythai35@gmail.com>
2017-09-12 09:28:33 -0500

    update spa service

---
+++ Chapter3/src/youDoIt/SpaService.java
@@ -0,0 +1,31 @@
+package youDoIt;
+
+public class SpaService {
+		// TODO Auto-generated method stub
+		private String serviceDescription;
+		private double price;
+	
+	
+	
+	public void setServiceDescription(String service)
+	{
+		serviceDescription = service;
+		
+	}
+	
+	public void setPrice(double pr)
+	{
+		price = pr;
+	}
+	
+	public String getServiceDescription()
+	{
+		return serviceDescription;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+
+}
