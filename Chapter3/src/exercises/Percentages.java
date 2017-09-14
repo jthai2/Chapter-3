07cb6d2ce6ec35d628a407e4ab79c81e48d6e63f
jthai2 <jimmythai35@gmail.com>
2017-09-14 08:59:24 -0500

    update percentages

--- Chapter3/src/exercises/Percentages.java
+++ Chapter3/src/exercises/Percentages.java
@@ -1,10 +1,31 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Percentages {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double A;
+		double B;
+		double percentage;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter a number >> ");
+		A = keyboard.nextInt();
+		System.out.print("Enter another number >> ");
+		B = keyboard.nextInt();
+		
+		percentage = computePercent(A, B);
+		
+		System.out.println(A + " is " + percentage + " percent of " + B);
+	}
+	
+	public static double computePercent(double A, double B)
+	{
+		double percentage;
+		percentage = A * B / 100;
+		return percentage;
 	}
 
 }
