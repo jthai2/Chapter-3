10adaacb07315ae40423198269980251a2ad768d
jthai2 <jimmythai35@gmail.com>
2017-09-14 08:26:05 -0500

    update name

--- Chapter3/src/exercises/NumbersDemo2.java
+++ Chapter3/src/exercises/NumbersDemo2.java
@@ -2,7 +2,7 @@ package exercises;
 
 import java.util.Scanner;
 
-public class NumbersDemo {
+public class NumbersDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
