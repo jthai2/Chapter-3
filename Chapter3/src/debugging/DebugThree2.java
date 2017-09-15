5b845ba9901743922c3c9527fa78a85dd4fabba2
jthai2 <jimmythai35@gmail.com>
2017-09-15 17:17:32 -0500

    update debugthree2

--- Chapter3/src/debugging/DebugThree2.java
+++ Chapter3/src/debugging/DebugThree2.java
@@ -1,10 +1,22 @@
 package debugging;
 
-public class DebugThree2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class DebugThree2
+{
+   public static void main(String args[])
+   {
+      int a = 2, b = 5, c = 10;
+      add(a, b);
+      add(b, c);
+      subtract(c, a);            
+   }
+   public static void add(int a, int  b)
+   {
+      System.out.println("The sum of " + a +
+         " and " + b + " is " + (a + b));
+   }
+   public static void subtract(int a, int b)
+   {
+      System.out.println("The difference between " +
+        a + " and " + b + " is " +  (a - b));
+   }
 }
