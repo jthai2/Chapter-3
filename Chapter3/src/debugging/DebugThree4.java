fb8026dc6fba29f0aed4b7a127a136df54783ac6
jthai2 <jimmythai35@gmail.com>
2017-09-15 17:33:31 -0500

    update

--- Chapter3/src/debugging/DebugThree4.java
+++ Chapter3/src/debugging/DebugThree4.java
@@ -1,10 +1,19 @@
 package debugging;
 
-public class DebugThree4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class DebugThree4
+{
+   public static void main(String args[])
+   {
+      int myCredits = 13;
+      int yourCredits = 17;
+      double rate = 75.84;
+      System.out.println("My tuition:");
+      tuitionBill(myCredits, rate);
+      System.out.println("Your tuition:");
+      tuitionBill(yourCredits, rate);
+   }
+   public static void tuitionBill(int c, double r)
+   {
+      System.out.println("Total due $" + (r*c));
+   }
 }
