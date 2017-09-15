fa199320fa1a1bab4d4e3fbdd9ec5011c4406633
jthai2 <jimmythai35@gmail.com>
2017-09-15 17:27:04 -0500

    update debug

--- Chapter3/src/debugging/DebugThree3.java
+++ Chapter3/src/debugging/DebugThree3.java
@@ -1,10 +1,24 @@
 package debugging;
 
-public class DebugThree3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.util.Scanner;
+public class DebugThree3
+{
+   public static void main(String args[])
+   {
+      String name;
+      name = getName();
+      displayGreeting(name);           
+   }
+   public static String getName()
+   {
+      String name;
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter name >> ");
+      name = input.nextLine();
+      return name;
+   }
+   public static void displayGreeting(String name)
+   {
+      System.out.println("Hello, " + name + "!");
+   }
 }
