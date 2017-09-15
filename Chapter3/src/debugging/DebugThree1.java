2af0cb0e21d633adfbaf9423b2d9d6f0ea30070a
jthai2 <jimmythai35@gmail.com>
2017-09-15 17:05:41 -0500

    update debug

--- Chapter3/src/debugging/DebugThree1.java
+++ Chapter3/src/debugging/DebugThree1.java
@@ -2,23 +2,24 @@
 package debugging;
 
 import java.text.DecimalFormat;
-
+//This class calculates a waitperson's tip as 15% of the bill
 public class DebugThree1
 {
-   public static void main(String args[])
-   {
-      double myCheck = 50.00;
-      double yourCheck = 19.95;
-      System.out.println("Tips are");
-      calcTip(myCheck);
-      calcTip(yourCheck);
-    }
-    public static void calcTip(double bill)
-    {
-      double tip;
-      final double RATE = 0.15;
-      tip = bill * RATE;
-      DecimalFormat df = new DecimalFormat("0.00");
-      System.out.println("The tip should be at least " + (df.format(tip)));
-    }
+public static void main(String args[])
+{
+   double myCheck = 50.00;
+   double yourCheck = 19.95;
+   System.out.println("The bill amounts are $" + myCheck + " and $" + yourCheck);
+   calcTip(myCheck);
+   calcTip(yourCheck);
+ }
+	   
+ public static void calcTip(double bill)
+ {
+   final double RATE = 0.15;
+   double tip = bill * RATE;
+   
+   DecimalFormat df = new DecimalFormat("0.00");
+   System.out.println("The tip should be at least $" + (df.format(tip)));
+ }
 }
